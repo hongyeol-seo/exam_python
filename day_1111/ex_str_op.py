b2aa5278b19f8ee9b896fcb04692a7fb07af1763
hongyeol <napolehong92@naver.com>
2022-11-11 19:12:45 +0900

    문자열 str

--- day_1111/ex_str_op.py
+++ day_1111/ex_str_op.py
@@ -5,11 +5,31 @@
 name = "Hong"
 gender = "M"
 
-# 산술연살 수행 -------------------------------------
+# 산술연산 수행 -------------------------------------
 # 덧셈 : str + str = 문자열 연결
 print(f"{name} + {gender} = {name+gender}")
 # print(f"{name} + {2022} = {name+2022}") #문자열끼리만 덧셈 가능
 # 오직 str + int 안됨! 오직 str + str만 됨
 print(f"{name} + {str(2022)} = {name+str(2022)}")
+1
+#뺄셈 : str - str = 지원되지 않는 기능
+# print(f"{name} - {gender} = {name-gender}") #unsupported operand 이 연산 기능을 지원하지 않는다.
 
+#곱셉
+# print(f"{name} * {gender} = {name*gender}") #can't multiply sequence #곱하기는 int랑만 가능!
+print(f'{name} * 5 = {name * 5}') #5번 반복!
 
+# 나머지(%) => d는 정수, f는 실수, s는 문자열 
+
+#[실습] --------------------------------------------
+# 출력 => =====================================
+#                    My Program
+#         =====================================
+
+print("\t\t\t\t"+"="*25)
+print(f'\t\t\t\t\tMy Program')
+print("\t\t\t\t"+"="*25)
+
+#한 줄로 만들기!
+
+print("="*25 + '\n\tMy Program\n' + "="*25)
