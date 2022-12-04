350635f0bc8a237327e6749ae885d1156c7f794e
hongyeol <napolehong92@naver.com>
2022-12-04 20:37:31 +0900

    221204 코딩테스트공부

--- test/221201__코딩테스트문제.py
+++ test/221201__코딩테스트문제.py
@@ -127,6 +127,7 @@ def measure(n):
 
 measure(int(input("구하고 싶은 수 : ")))
 
+
 # 8. 입력 받은 메시지 중에서 중복 없이 숫자만 출력하는 함수를 만들어 주세요.
 
 msg = ""
@@ -187,7 +188,7 @@ while check :
         
     print("=",num)
 
-# 11. 구구단 n단부터 m단까지를 반복문 1개로 구현하는 함수를 생성하세요.
+# 13. 구구단 n단부터 m단까지를 반복문 1개로 구현하는 함수를 생성하세요.
 
 #시작 구구단 종료구구단
 
@@ -218,3 +219,206 @@ def gugudan(s,e):
 
 gugudan(2,6)
     
+
+
+
+check = True 
+incheck = True #출력체크여부
+num = 7 #입력값 
+checkNum = num
+sum = 1 #합계저장
+
+while check :
+
+    if num == 0:
+        print(f'{num}! => {num}')
+        check = False
+        continue
+    elif num >= 1 and incheck :
+        print(f'{num}! =>',end=" ")
+        incheck = False
+        continue
+    elif checkNum >= 1 :
+        if checkNum > 1 :
+            print(checkNum, "*", end=" ")
+            sum *= checkNum 
+            checkNum -= 1
+            continue
+        else :
+            print(checkNum,end=" ")
+            sum *= checkNum 
+            checkNum -= 1
+            continue
+
+    else :
+        print(f"= {sum}")
+        break
+        
+# 11. 입력받은 숫자 범위 안에서 소수(Prime Number)를 찾아서 반환하는 함수를 생성하세요.
+
+# 입력은 함수에 포함하지 않음
+# - 소수(Prime Number)란? 1보다 큰 수 중 1과 자기 자신만을 약수로 가지는 수
+
+n=10
+
+def isPrime(a):
+  if(a<2):
+    return False
+  for i in range(2,a):
+    if(a%i==0):
+      return False
+  
+  return True
+
+for i in range(n+1):
+  if(isPrime(i)):
+    print(i)
+
+
+#12. 아래 데이터를 저장합니다. 그리고 과목별 최고점수, 최저점수 출력하세요.
+
+data = {"국어":[90,82,77,94,78],"수학":[89,71,100,82,99],"윤리":[98,80,92,93,91],"국사":[99,91,90,71,83]}
+
+arr= list(data.keys())
+
+print(f'[{arr[0]}] 최고점수 : {max(data[arr[0]])}, 최저점수`{min(data[arr[0]])}')
+print(f'[{arr[1]}] 최고점수 : {max(data[arr[1]])}, 최저점수`{min(data[arr[1]])}')
+print(f'[{arr[2]}] 최고점수 : {max(data[arr[2]])}, 최저점수`{min(data[arr[2]])}')
+print(f'[{arr[3]}] 최고점수 : {max(data[arr[3]])}, 최저점수`{min(data[arr[3]])}')
+
+
+# 14. 입력받은 숫자에서 천자리, 백자리, 십자리, 일자리를 출력하세요.
+
+n = 12345
+
+num = len(str(n))
+
+if num == 5:
+
+printf(" 천의 자리 : %d \n" , num/1000);
+num = num % 1000;
+printf(" 백의 자리 : %d \n" , num / 100);
+num = num % 100;
+
+
+n = 12345
+print("만의자리 : {}".format(int(n/10000))) if n/10000 > 1 else True
+print("천의자리 : {}".format(int(n/1000)/10)) if (n%10000)/1000 > 1 else True
+
+
+
+
+
+print("백의자리 : {}".format(int(n/100))) if n/100 > 1 else print("패스")
+
+a = 123
+print("백의자리 : %d\n"%(a/100))
+print("십의자리 : %d\n"%((a%100)/10))
+print("일의자리 : %d\n"%(a%10))
+
+
+
+# 11. 아래 그림처럼 나오도록 코드 작성하세요. 가장 긴 라인의 별의 수는 15개,
+#  전체 라인수도 15라인 입니다.
+
+for i in range(8):
+    print("{:^15}".format("*"*(2*i+1)))
+for i in range(7):
+    print("{:^15}".format("*"*(13-2*i)))
+
+for i in range(1,17,2) :
+    print("{0:^15}".format("*" * i))
+
+print("{0:^15}".format("*" * 4))
+print("{0:^15}".format("*" * 4))
+print("{0:^15}".format("Merry Christmas"))
+print("{0:^15}".format("2023"))
+
+# 17. 문자열 ‘Merry Christmas HaPPy New YEaR’에서 대문자는 소문자로, 소문자는 대문자로
+# 변환하여 출력하는 코드를 구현하세요.
+
+data = 'Merry Christmas HaPPy New YEaR'
+
+print(data.swapcase())
+
+#18. 6가지 연산 결과를 한꺼번에 반환하는 함수를 생성 후 호출하여 결과를 아래와 같이출력해 주세요.
+
+
+#19. 다양한 사람들로부터 개인정보를 입력받는 함수를 구현하세요.
+
+def saveInfo(**datas):
+    for key in datas.keys():
+        print(f"{key} => {datas.get(key)}")
+
+saveInfo(age=12, phone='010-2222-1111',job='학생', loc='대구')
+
+
+
+
+# def saveInfo(**infos):
+#     print(type(infos), len(infos), infos)
+#     for k,v in infos.items():
+#         print(f'{k}-{v}')
+
+#     for key in infos.keys():
+#         print(f'{key} => {infos.get(key)}')
+
+
+
+
+#20 [나의 계산기] 프로그램을 구현하세요.
+
+def choice():
+    print("1. 입력",end="  ")
+    print("2. 덧셈",end="  ")
+    print("3. 뺄셈",end="\n")
+    print("4. 곱셉",end="  ")
+    print("5. 나눗셈",end="  ")
+    print("6. 종료",end="\n")
+
+isCheck = False
+while True :
+    
+    choice()
+    data = input("메뉴선택 (1~6) : ").strip()
+    
+    if data == "6": #종료
+        print("나의 계산기 프로그램을 종료합니다.")
+        break
+
+    elif data == "1": #입력
+        if isCheck == False :
+            nums = input("숫자 2개 입력 : ").strip()
+            n1, n2 = nums.split(",")
+            n1, n2 = int(n1), int(n2)
+            isCheck = True
+        
+        else :
+            print("입력된 값이 있습니다.")
+
+    elif data == "2":
+        if isCheck:
+            print(f"{n1} + {n2} = {n1+n2}")
+        else :
+            print("입력된 데이터가 없습니다.")
+
+    elif data == "3":
+        if isCheck:
+            print(f"{n1} - {n2} = {n1-n2}")
+        else :
+            print("입력된 데이터가 없습니다.")
+    
+    elif data == "4":
+        if isCheck:
+            print(f"{n1} * {n2} = {n1*n2}")
+        else :
+            print("입력된 데이터가 없습니다.")
+    
+    elif data == "5":
+        if isCheck:
+            print(f"{n1} / {n2} = {n1*n2}")
+        else :
+            print("입력된 데이터가 없습니다.")
+
+    else:
+        print("메뉴 선택값이 없습니다.")
