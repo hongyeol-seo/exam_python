8ac7aa84388e49d3c13ab527ede762ea1717cf67
HongYeol Seo <napolehong92@naver.com>
2022-12-07 22:40:41 +0900

    221207_코딩데스트준비완료

--- test/221201__코딩테스트문제.py
+++ test/221201__코딩테스트문제.py
@@ -2,108 +2,112 @@
 
 def sortArr(arr):
     # arr.sort(reverse=True)
-    arr = sorted(arr,reverse=True) #default 오름차순, reverse=True 내림차순
+    arr = sorted(arr, reverse=True)  # default 오름차순, reverse=True 내림차순
     print(arr)
-    print('가장 높은 문자열 : {}, 가장 낮은 문자열 : {}'.format(arr[0],arr[-1]))
+    print('가장 높은 문자열 : {}, 가장 낮은 문자열 : {}'.format(arr[0], arr[-1]))
 
-msg=['Good', 'child', 'Zoo', 'apple', 'Flower', 'zero']
+
+msg = ['Good', 'child', 'Zoo', 'apple', 'Flower', 'zero']
 
 sortArr(msg)
 
 # 2. 문자열을 입력하면 UTF-8로 인코딩된 값을 아래와 같이 출력된 함수를 구현해 주세요.
 
-#hex 16진수 0x
-#oct 8진수 0o
-#bin 2진수 0b
+# hex 16진수 0x
+# oct 8진수 0o
+# bin 2진수 0b
+
 
 def printData(n):
     """진수변환함수"""
     print("{:=^10}".format("16진수"))
     for i in n:
-        print(hex(ord(i)),end="")
+        print(hex(ord(i)), end="")
 
     print("{:=^10}".format("8진수"))
     for j in n:
-        print(oct(ord(j)),end="")
+        print(oct(ord(j)), end="")
 
     print("{:=^10}".format("2진수"))
     for j in n:
-        print(bin(ord(j)),end="")
-    
+        print(bin(ord(j)), end="")
+
+
 data = "가나다"
 printData(data)
 
-#3. 숫자와 콤마로만 이루어진 문자열 data가 주어진다. 이때, data에 포함되어있는 자연수의 합과 가장 작은 수, 가장 큰 수를 출력하는 함수를 구현하세요.
+# 3. 숫자와 콤마로만 이루어진 문자열 data가 주어진다. 이때, data에 포함되어있는 자연수의 합과 가장 작은 수, 가장 큰 수를 출력하는 함수를 구현하세요.
+
 
 def printData(m):
-    data = list(m.replace(",",""))
+    data = list(m.replace(",", ""))
     for i in range(len(data)):
         data[i] = int(data[i])
 
-    print('{}의 합 : {}, 가장 큰 수 {}, 가장 작은 수 {}'.format(m, sum(data),max(data),min(data)))
+    print('{}의 합 : {}, 가장 큰 수 {}, 가장 작은 수 {}'.format(
+        m, sum(data), max(data), min(data)))
+
 
 printData('123,42,98,18')
 
-data='123,42,98,18'
-data = list(data.replace(",",""))
+data = '123,42,98,18'
+data = list(data.replace(",", ""))
 
 for i in range(len(data)):
     data[i] = int(data[i])
 
-print('123,42,98,18의 합 : {}, 가장 큰 수 {}, 가장 작은 수 {}'.format(sum(data),max(data),min(data)))
+print('123,42,98,18의 합 : {}, 가장 큰 수 {}, 가장 작은 수 {}'.format(
+    sum(data), max(data), min(data)))
 
-data="1,234"
-data=list(data.replace(",",""))
+data = "1,234"
+data = list(data.replace(",", ""))
 for i in range(len(data)):
     data[i] = int(data[i])
 
-print('1,234의 합 : {}, 가장 큰 수 {}, 가장 작은 수 {}'.format(sum(data),max(data),min(data)))
+print('1,234의 합 : {}, 가장 큰 수 {}, 가장 작은 수 {}'.format(
+    sum(data), max(data), min(data)))
 
 # 4. 2, 4, 8 게임은 숫자의 끝 자리가 2, 4, 8로 끝나는 숫자의 경우 다른 문자로 출력하는 게임으로 아래 조건을 만족하도록 구현하자.
 
-n = int(input("숫자를 입력하세요 : ").strip())
 
-for i in range(1,n+1):
+def strPrint(data):
 
-    count = 0
-    for j in str(i):
-        if "2" in j or "4" in j or "8" in j :
-            count+=1
-            
-    if count >= 1:
-        if i % 20 == 0:
-            print("#", end="\n")
+    for i in range(1, data+1):
+        j = str(i)
+        if j[-1] == "2" or j[-1] == "4" or j[-1] == "8":
+            print("#", end="")
         else:
-            print("#", end=" ")        
-    else :
-        if i % 20 == 0:
-            print(i,end="\n")
-        else :
-            print(i,end=" ")
+            print(i, end="")
 
-#5. 월(Month)을 입력 받아 해당 월(Month)의 영어와 계절을 출력하는 코드를 작성하세요
+        if i % 20 == 0:
+            print("")
+
+
+strPrint(100)
+
+# 5. 월(Month)을 입력 받아 해당 월(Month)의 영어와 계절을 출력하는 코드를 작성하세요
 
 # month = int(input("좋아하는 월 입력 : "))
 month = 15
 
-monDict = {1:["Jan.","Winter"],
-2:["Feb.","Witer"],
-3:["Mar.","Spring"],
-4:["Apr.","Spring"],
-5:["May","Spring"],
-6:["Jun.","Summer"],
-7:["jul.","Summer"],
-8:["Aug.","Summer"],
-9:["Sep","autumn"],
-10:["Oct","autumn"],
-11:["Nov","autumn"],
-12:["Dec.","Winter"]}
+monDict = {1: ["Jan.", "Winter"],
+           2: ["Feb.", "Witer"],
+           3: ["Mar.", "Spring"],
+           4: ["Apr.", "Spring"],
+           5: ["May", "Spring"],
+           6: ["Jun.", "Summer"],
+           7: ["jul.", "Summer"],
+           8: ["Aug.", "Summer"],
+           9: ["Sep", "autumn"],
+           10: ["Oct", "autumn"],
+           11: ["Nov", "autumn"],
+           12: ["Dec.", "Winter"]}
 
-if month in monDict :
+if month in monDict:
     for i in monDict[month]:
-        print(i,end=" ")
+        print(i, end=" ")
 
-else :
+else:
     print("존재하지 않는 월입니다.")
 
 # 6. 숫자와 화폐단위를 입력 받아 세자리 마다 쉼표(,) 찍어서 출력하는 기능을 구현하세요
@@ -112,24 +116,26 @@ data = "   1234567,  $"
 
 data = data.strip()
 print(data)
-data = data.replace(" ","")
-data = data.replace(",","")
+data = data.replace(" ", "")
+data = data.replace(",", "")
 won = data[-1]
 # print(format(int(data[:-1]),",d"),end="") #
-print("{:,}{}".format(int(data[:-1]),won))
-#format(int(data[:-1]),",d"),end="") #
+print("{:,}{}".format(int(data[:-1]), won))
+# format(int(data[:-1]),",d"),end="") #
 # print(won)
 print(format(12345678910, ',d'))
 
 # 7. 입력받은 정수에 대한 약수를 출력하는 함수를 만들어 주세요.
 
+
 def measure(n):
     arr = []
-    for i in range(1,n+1):
-        if n%i == 0 :
+    for i in range(1, n+1):
+        if n % i == 0:
             arr.append(str(i))
             data = "".join(arr)
     print(f'{n}의 약수 :{data}')
+
 
 measure(int(input("구하고 싶은 수 : ")))
 
@@ -166,10 +172,10 @@ nDay = int(nDay)
 year = nYear - mYear
 koreaAge = year+1
 
-if mMonth > nMonth :
+if mMonth > nMonth:
     year -= 1
 
-elif mMonth == nMonth and mDay > nDay :
+elif mMonth == nMonth and mDay > nDay:
     year -= 1
 
 print(f"한국나이는 {koreaAge}살입니다.")
@@ -181,14 +187,14 @@ print(f"외국나이는 {year}살입니다.")
 # 팩토리얼(Factorial)을 while반복문으로 구현해 주세요. 팩토리얼 수를 입력 받아서 팩토리얼 결과를 아래와 같이 출력하세요.
 
 inputNum = 7
-num = 0 
+num = 0
 check = True
 
-while check :
-    if inputNum == 0 :
+while check:
+    if inputNum == 0:
         check = False
         continue
-    
+
     print(str(inputNum)+"! =>", end="")
     while check:
         if inputNum == 0:
@@ -200,47 +206,48 @@ while check :
         print(inputNum, end=" ")
         if inputNum != 1 and inputNum != 0:
             print("*", end=" ")
-        
-    print("=",num)
+
+    print("=", num)
 
 
-check = True 
-incheck = True #출력체크여부
-num = 7 #입력값 
+check = True
+incheck = True  # 출력체크여부
+num = 7  # 입력값
 checkNum = num
-sum = 1 #합계저장
+sum = 1  # 합계저장
 
-while check :
+while check:
 
     if num == 0:
         print(f'{num}! => {num}')
         check = False
         continue
-    elif num >= 1 and incheck :
-        print(f'{num}! =>',end=" ")
+    elif num >= 1 and incheck:
+        print(f'{num}! =>', end=" ")
         incheck = False
         continue
-    elif checkNum >= 1 :
-        if checkNum > 1 :
+    elif checkNum >= 1:
+        if checkNum > 1:
             print(checkNum, "*", end=" ")
-            sum *= checkNum 
+            sum *= checkNum
             checkNum -= 1
             continue
-        else :
-            print(checkNum,end=" ")
-            sum *= checkNum 
+        else:
+            print(checkNum, end=" ")
+            sum *= checkNum
             checkNum -= 1
             continue
 
-    else :
+    else:
         print(f"= {sum}")
         break
 
 # 13. 구구단 n단부터 m단까지를 반복문 1개로 구현하는 함수를 생성하세요.
 
-#시작 구구단 종료구구단
+# 시작 구구단 종료구구단
 
-def gugudan(s,e):
+
+def gugudan(s, e):
 
     check = True
     strartNum = s
@@ -249,9 +256,9 @@ def gugudan(s,e):
     num = 1
 
     while check:
-        if newStr <= endNum and num < 10: 
+        if newStr <= endNum and num < 10:
             print(f"{newStr} * {num} = {newStr*num}", end="\t")
-            newStr +=1 
+            newStr += 1
             # continue
 
         elif newStr > endNum and num < 10:
@@ -260,35 +267,39 @@ def gugudan(s,e):
             num += 1
             # continue
 
-        else :
+        else:
             check = False
-            #break
+            # break
 
-gugudan(2,6)
-    
+
+gugudan(2, 6)
+
 # 11. 입력받은 숫자 범위 안에서 소수(Prime Number)를 찾아서 반환하는 함수를 생성하세요.
 # 입력은 함수에 포함하지 않음
 # - 소수(Prime Number)란? 1보다 큰 수 중 1과 자기 자신만을 약수로 가지는 수
 
-n=50
+n = 50
+
 
 def isPrime(a):
-  if(a<2):
-    return False
-  for i in range(2,a):
-    if(a%i==0):
-      return False
-  
-  return True
+    if (a < 2):
+        return False
+    for i in range(2, a):
+        if (a % i == 0):
+            return False
+
+    return True
+
 
 for i in range(n+1):
-  if(isPrime(i)):
-    print(i,end=" ")
+    if (isPrime(i)):
+        print(i, end=" ")
 
 
-#12. 아래 데이터를 저장합니다. 그리고 과목별 최고점수, 최저점수 출력하세요.
-data = {"국어":[90,82,77,94,78],"수학":[89,71,100,82,99],"윤리":[98,80,92,93,91],"국사":[99,91,90,71,83]}
-arr= list(data.keys())
+# 12. 아래 데이터를 저장합니다. 그리고 과목별 최고점수, 최저점수 출력하세요.
+data = {"국어": [90, 82, 77, 94, 78], "수학": [89, 71, 100, 82, 99],
+        "윤리": [98, 80, 92, 93, 91], "국사": [99, 91, 90, 71, 83]}
+arr = list(data.keys())
 
 print(f'[{arr[0]}] 최고점수 : {max(data[arr[0]])}, 최저점수`{min(data[arr[0]])}')
 print(f'[{arr[1]}] 최고점수 : {max(data[arr[1]])}, 최저점수`{min(data[arr[1]])}')
@@ -300,14 +311,14 @@ print(f'[{arr[3]}] 최고점수 : {max(data[arr[3]])}, 최저점수`{min(data[ar
 
 a = 12345
 
-a % 1000
+a % 10
 
 # print("백의자리 : %d\n"%(a/1000) if (a/1000) > 0 else False)
 print("만의자리 {}:".format((a//10000) if (a//10000) >= 0 else ""))
-print("천의자리 {}:".format(((a%10000)//1000) if ((a%10000)//1000) > 0 else ""))
-print("백의자리 {}:".format(((a%1000)//100) if (a%1000//100) > 0 else ""))
-print("십의자리 {}:".format(((a%100)//10) if (a%100//10) > 0 else ""))
-print("일의자리 {}:".format(((a%10)) if (a%10) > 0 else ""))
+print("천의자리 {}:".format(((a % 10000)//1000) if ((a % 10000)//1000) > 0 else ""))
+print("백의자리 {}:".format(((a % 1000)//100) if (a % 1000//100) > 0 else ""))
+print("십의자리 {}:".format(((a % 100)//10) if (a % 100//10) > 0 else ""))
+print("일의자리 {}:".format(((a % 10)) if (a % 10) > 0 else ""))
 
 # 11. 아래 그림처럼 나오도록 코드 작성하세요. 가장 긴 라인의 별의 수는 15개,
 #  전체 라인수도 15라인 입니다.
@@ -317,7 +328,7 @@ print("일의자리 {}:".format(((a%10)) if (a%10) > 0 else ""))
 # for i in range(7):
 #     print("{:^15}".format("*"*(13-2*i)))
 
-for i in range(1,17,2) :
+for i in range(1, 17, 2):
     print("{0:^15}".format("*" * i))
 
 print("{0:^15}".format("*" * 4))
@@ -332,41 +343,44 @@ data = 'Merry Christmas HaPPy New YEaR'
 
 print(data.swapcase())
 
-#18. 6가지 연산 결과를 한꺼번에 반환하는 함수를 생성 후 호출하여 결과를 아래와 같이출력해 주세요.
+# 18. 6가지 연산 결과를 한꺼번에 반환하는 함수를 생성 후 호출하여 결과를 아래와 같이출력해 주세요.
+
 
 def fourCalc(num1, num2):
-    addValue=num1+num2
-    subValue=num1-num2
-    mulValue=num1*num2
-    divValue=num1/num2 if num2>0 else 0
-    remain = num1%num2
+    addValue = num1+num2
+    subValue = num1-num2
+    mulValue = num1*num2
+    divValue = num1/num2 if num2 > 0 else 0
+    remain = num1 % num2
     remain2 = num1//num2
 
-    return addValue, subValue, mulValue, divValue,remain,remain2
+    return addValue, subValue, mulValue, divValue, remain, remain2
 
 
-print('덧셈 값 : {}, \n뺄셈 값 : {}, \n곱셉 값 : {},\n나눗셈 값 {}, \n나머지 값 : {}, \n몫 : {}'.format(fourCalc(4,3)[0],fourCalc(4,3)[1],fourCalc(4,3)[2],fourCalc(4,3)[3],fourCalc(4,3)[4],fourCalc(4,3)[5]))
+print('덧셈 값 : {}, \n뺄셈 값 : {}, \n곱셉 값 : {},\n나눗셈 값 {}, \n나머지 값 : {}, \n몫 : {}'.format(fourCalc(4, 3)[
+      0], fourCalc(4, 3)[1], fourCalc(4, 3)[2], fourCalc(4, 3)[3], fourCalc(4, 3)[4], fourCalc(4, 3)[5]))
 
 
-#19. 다양한 사람들로부터 개인정보를 입력받는 함수를 구현하세요.
+# 19. 다양한 사람들로부터 개인정보를 입력받는 함수를 구현하세요.
 
 def saveInfo(**datas):
     for key in datas.keys():
         if datas.get(key) != "":
             print(f"{key} => {datas.get(key)}")
 
+
 id = input("아이디를 입력하세요 : ").strip()
 name1 = input("이름을 입력하세요 : ").strip()
 age = input("나이를 입력하세요 : ").strip()
 phone = input("번호를 입력하세요 : ").strip()
-job= input("직업을 입력하세요.").strip()
-loc= input("지역을 입력하세요 : ").strip()
+job = input("직업을 입력하세요.").strip()
+loc = input("지역을 입력하세요 : ").strip()
 
 # phone = '010-2222-1111'
 # job='학생'
 # loc='대구'
 
-saveInfo(id2=id,name2=name1,age2=age,phone2=phone,job2=job,loc2=loc)
+saveInfo(id2=id, name2=name1, age2=age, phone2=phone, job2=job, loc2=loc)
 
 # def saveInfo(**infos):
 #     print(type(infos), len(infos), infos)
@@ -376,58 +390,60 @@ saveInfo(id2=id,name2=name1,age2=age,phone2=phone,job2=job,loc2=loc)
 #     for key in infos.keys():
 #         print(f'{key} => {infos.get(key)}')
 
-#20 [나의 계산기] 프로그램을 구현하세요.
+# 20 [나의 계산기] 프로그램을 구현하세요.
+
 
 def choice():
-    print("1. 입력",end="  ")
-    print("2. 덧셈",end="  ")
-    print("3. 뺄셈",end="\n")
-    print("4. 곱셉",end="  ")
-    print("5. 나눗셈",end="  ")
-    print("6. 종료",end="\n")
+    print("1. 입력", end="  ")
+    print("2. 덧셈", end="  ")
+    print("3. 뺄셈", end="\n")
+    print("4. 곱셉", end="  ")
+    print("5. 나눗셈", end="  ")
+    print("6. 종료", end="\n")
+
 
 isCheck = False
-while True :
-    
+while True:
+
     choice()
     data = input("메뉴선택 (1~6) : ").strip()
-    
-    if data == "6": #종료
+
+    if data == "6":  # 종료
         print("나의 계산기 프로그램을 종료합니다.")
         break
 
-    elif data == "1": #입력
-        if isCheck == False :
+    elif data == "1":  # 입력
+        if isCheck == False:
             nums = input("숫자 2개 입력 : ").strip()
             n1, n2 = nums.split(",")
             n1, n2 = int(n1), int(n2)
             isCheck = True
-        
-        else :
+
+        else:
             print("입력된 값이 있습니다.")
 
     elif data == "2":
         if isCheck:
             print(f"{n1} + {n2} = {n1+n2}")
-        else :
+        else:
             print("입력된 데이터가 없습니다.")
 
     elif data == "3":
         if isCheck:
             print(f"{n1} - {n2} = {n1-n2}")
-        else :
+        else:
             print("입력된 데이터가 없습니다.")
-    
+
     elif data == "4":
         if isCheck:
             print(f"{n1} * {n2} = {n1*n2}")
-        else :
+        else:
             print("입력된 데이터가 없습니다.")
-    
+
     elif data == "5":
         if isCheck:
             print(f"{n1} / {n2} = {n1*n2}")
-        else :
+        else:
             print("입력된 데이터가 없습니다.")
 
     else:
@@ -437,31 +453,28 @@ while True :
 # 15. 정수, 실수, 논리, 문자열 등 데이터 입력 시 모두 덧셈한 결과 출력하는 함수를 생성하세요.
 
 def addData(*datas):
-    if len(datas)>0:  
-        if isinstance(datas[0], str): 
+    if len(datas) > 0:
+        if isinstance(datas[0], str):
             if datas[0].isdecimal():
-                total= 0
-            else :
-                total= ''    
+                total = 0
+            else:
+                total = ''
         else:
-            total= 0
+            total = 0
 
         for data in datas:
             # print(type(data))
             if type(data) == str and data.isdecimal():
                 total += int(data)
-            else :
-                total +=data
+            else:
+                total += data
 
         return total
 
+
 print(addData("2", "9", "3"))
-print(addData(1,3,5))
+print(addData(1, 3, 5))
 # print(addData(1,'a',5))
 print(addData(True, True, False, False, True))
 print(addData('A'))
-print(addData('A','BC','Good'))
-
-
-
-
+print(addData('A', 'BC', 'Good'))
---
+++ test/221207 코딩테스트 모의시험.py
@@ -0,0 +1,420 @@
+# 1
+from datetime import datetime
+msg = ['Good', 'child', 'Zoo', 'apple', 'Flower', 'zero']
+
+
+def listSort(data):
+    data.sort(reverse=True)
+    print(data)
+    print(f"가장 높은 문자열 {data[0]}, 가장 낮은 문자열 {data[-1]}")
+
+
+listSort(msg)
+
+# 2
+data = "가나다"
+
+
+def strEncoding(data):
+    print("가나다의 16진수 인코딩 {}{}{}".format(
+        hex(ord(data[0])), hex(ord(data[1])), hex(ord(data[2]))))
+    print("가나다의 8진수 인코딩 {}{}{}".format(
+        oct(ord(data[0])), oct(ord(data[1])), oct(ord(data[2]))))
+    print("가나다의 2진수 인코딩 {}{}{}".format(
+        bin(ord(data[0])), bin(ord(data[1])), bin(ord(data[2]))))
+
+
+strEncoding(data)
+
+# 3
+
+
+def listSum(data):
+    data1 = list(data.replace(",", ""))
+
+    for i in range(len(data1)):
+        data1[i] = int(data1[i])
+
+    print(f"{data}의 합 : {sum(data1)},가장 큰 수 : {max(data1)}, 가장 작은 수 {min(data1)} ")
+
+
+data = '123,42,98,18'
+listSum(data)
+
+data = '1,234'
+listSum(data)
+
+# 4
+
+
+def strPrint(data):
+
+    for i in range(1, data+1):
+        j = str(i)
+        if j[-1] == "2" or j[-1] == "4" or j[-1] == "8":
+            print("#", end="")
+        else:
+            print(i, end="")
+
+        if i % 20 == 0:
+            print("")
+
+
+strPrint(int(input("정수 입력 : ")))
+
+# 5
+
+
+def favorMonth(num):
+    month = {
+        1: ["Jan.", "Winter"],
+        2: ["Fab.", "Wither"],
+        3: ["Mar.", "Spring"],
+        4: ["Apr.", "Spring"],
+        5: ["May", "Spring"],
+        6: ["Jun.", "Summer"],
+        7: ["Jul.", "Summer"],
+        8: ["Aug.", "Summer"],
+        9: ["Sep.", "Fall"],
+        10: ["OCt.", "Fall"],
+        11: ["Nov.", "Fall"],
+        12: ["Dec.", "Winter"]}
+
+    if num in list(month.keys()):
+        for i in month[num]:
+            print(i, end=" ")
+    else:
+        print("존재하지 않는 월입니다.")
+
+
+favorMonth(int(input("좋아하는 월 입력 : ")))
+
+# 6
+
+
+def addWon(n):
+    n = n.strip()
+    n = n.replace(",", "")
+    n, won = n.split(" ")
+    print(format(int(n), ","), won)
+
+
+n = "    1234567, $"
+addWon(n)
+n = "907, W"
+addWon(n)
+
+# 7
+# 함수
+
+
+def div(n):
+    arr = []
+    for i in range(1, n+1):
+        if n % i == 0:
+            arr.append(str(i))
+
+    print(f'{n}의 약수 : {" ".join(arr)}')
+
+
+div(8)
+
+# 8
+# 함수
+
+
+def outString(data):
+    s1 = set()
+    for i in data:
+        if i.isdecimal():
+            s1.add(int(i))
+    s1 = list(s1)
+    for j in s1:
+        print(j, end=" ")
+
+
+msg = "Happy New Year 2023"
+outString(msg)
+msg = '홍길동 010-1111-2222'
+outString(msg)
+
+# 9
+
+
+def yourAge(data):
+
+    tYear = datetime.today().year
+    tMonth = datetime.today().month
+    tDay = datetime.today().day
+
+    year, month, day = data[:-1].split(".")
+    print(tYear, type(tYear))
+
+    age = tYear - int(year)
+    KoeranAge = age + 1
+
+    if tMonth < int(month):
+        age -= 1
+    elif tMonth == int(month) and tDay < int(day):
+        age -= 1
+
+    print(f"한국나이 {KoeranAge}")
+    print(f'외국나이 {age}')
+
+
+n = '2000.12.31.'
+yourAge(n)
+
+# 10
+
+
+def factory(n):
+    sum = 1
+    if n == 0:
+        print(f"{n}! => {n}", end=" ")
+    else:
+        print(f"{n}! =>", end=" ")
+        for i in reversed(range(1, n+1)):
+            sum *= i
+            if i > 1:
+                print(f"{i} * ", end="")
+            else:
+                print(f"{i} = {sum}")
+
+
+factory(0)
+
+# 11
+# 다시 한 번 더 복습!
+
+
+def isPrime(n):
+    if n < 2:
+        return False
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+
+    return True
+
+
+# data = int(input("데이터를 입력해주세요. : "))
+data = 50
+
+for i in range(data+1):
+    if isPrime(i):
+        print(i, end=" ")
+
+# 12
+
+data = {"국어": [90, 82, 77, 94, 78], "수학": [89, 71, 100, 82, 99],
+        "윤리": [98, 80, 92, 93, 91], "국사": [99, 91, 90, 71, 83]}
+keyList = list(data.keys())
+
+print(
+    f"{keyList[0]} 최고점수 : {max(data[keyList[0]])}, 최저점수 : {min(data[keyList[0]])}")
+print(
+    f"{keyList[1]} 최고점수 : {max(data[keyList[1]])}, 최저점수 : {min(data[keyList[1]])}")
+print(
+    f"{keyList[2]} 최고점수 : {max(data[keyList[2]])}, 최저점수 : {min(data[keyList[2]])}")
+print(
+    f"{keyList[3]} 최고점수 : {max(data[keyList[3]])}, 최저점수 : {min(data[keyList[3]])}")
+
+# 13
+# 함수
+
+
+def gugudan(n, m):
+
+    strNum = n
+    endNum = m
+    line = 1
+    check = True
+    strCheck = True
+
+    while check:
+        if strCheck and strNum <= endNum:
+            print(f"  --{strNum}단--  ", end="\t")
+            strNum += 1
+            continue
+
+        elif strCheck and strNum > endNum:
+            strNum = n
+            strCheck = False
+            print("")
+
+        if strNum <= endNum and line < 10:
+            print(f"{strNum} * {line} = {strNum*line}", end="\t")
+            strNum += 1
+            # continue
+
+        if strNum <= endNum and line < 10:
+            print(f"{strNum} * {line} = {strNum*line}", end="\t")
+            strNum += 1
+            # continue
+        elif strNum > endNum and line < 10:
+            print("")
+            strNum = n
+            line += 1
+            # continue
+        else:  # strNum > endNum and line > 10:
+            check = False
+            # break
+
+# data = input("시작구구단, 종료구구단 : ")
+
+
+gugudan(3, 7)
+
+# 14
+
+a = 12345
+
+print("만의 자리 %s" % ((a//10000)))
+print("천의 자리 %s" % ((a % 10000//1000)))
+print("백의 자리 %s" % ((a % 1000//100)))
+print("십의 자리 %s" % ((a % 100//10)))
+print("일의 자리 %s" % ((a % 10)))
+
+print("만의자리 {}:".format((a//10000) if (a//10000) >= 0 else ""))
+print("천의자리 {}:".format(((a % 10000)//1000) if ((a % 10000)//1000) > 0 else ""))
+print("백의자리 {}:".format(((a % 1000)//100) if (a % 1000//100) > 0 else ""))
+print("십의자리 {}:".format(((a % 100)//10) if (a % 100//10) > 0 else ""))
+print("일의자리 {}:".format(((a % 10)) if (a % 10) > 0 else ""))
+
+# 15
+
+
+def addData(*data):
+    if len(data) > 0:
+        if isinstance(data[0], str):
+            print("앞에 숫자", data[0])
+            if data[0].isdecimal():
+                sum = 0
+
+            else:
+                sum = ""
+
+        else:
+            sum = 0
+
+        for i in data:
+            if type(i) == str and i.isdecimal():
+                sum += int(i)
+            else:
+                sum += i
+
+        print(sum)
+
+    else:
+        print(None)
+
+
+addData()
+addData(1, 3, 5)
+# addData(True, True, False, False, True)
+# addData('A')
+# addData('A', 'BC', 'Good')
+# addData("2", "9", "3")
+
+# 16
+
+for i in range(1, 9):
+    print("{:^15}".format("*"*((2*i)-1)))
+
+print("{:^15}".format("****"))
+print("{:^15}".format("****"))
+print("{:^15}".format("Merry Chiristmas"))
+print("{:^15}".format("2023"))
+
+# 17
+
+msg = 'Merry Christmas Happy New Year'
+
+newString = [i.upper() if i.islower() else i.lower() for i in msg]
+
+print("".join(newString))
+
+# 18
+
+
+def fourCalc(num1, num2):
+    addValue = num1+num2
+    subValue = num1-num2
+    mulValue = num1*num2
+    divValue = num1/num2 if num2 > 0 else 0
+    remain = num1 % num2
+    remain2 = num1//num2
+
+    return addValue, subValue, mulValue, divValue, remain, remain2
+
+
+print('덧셈 값 : {}, \n뺄셈 값 : {}, \n곱셉 값 : {},\n나눗셈 값 {}, \n나머지 값 : {}, \n몫 : {}'.format(fourCalc(4, 3)[
+      0], fourCalc(4, 3)[1], fourCalc(4, 3)[2], fourCalc(4, 3)[3], fourCalc(4, 3)[4], fourCalc(4, 3)[5]))
+
+# 19 #복습
+
+
+def saveInfo(**datas):
+    for i in datas.keys():
+        if datas.get(i) != "":
+            print(f"{i} => {datas[i]}")
+
+
+id = input("아이디를 입력하세요 : ").strip()
+name = input("이름을 입력하세요 : ").strip()
+age = input("나이를 입력하세요 : ").strip()
+phone = input("번호를 입력하세요 : ").strip()
+job = input("직업을 입력하세요.").strip()
+loc = input("지역을 입력하세요 : ").strip()
+
+saveInfo(id1=id, name1=name, age1=age, phone1=phone, job1=job, loc1=loc)
+
+# 20
+
+
+def menu():
+    print("="*40)
+    print("1. 입력", end="\t")
+    print("2. 덧셈", end="\t")
+    print("3. 뺄셈", end="\n")
+    print("4. 곱셈", end="\t")
+    print("5. 나눗셈", end="  ")
+    print("6. 종료")
+    print("="*40)
+
+
+isCheck = False
+while True:
+
+    menu()
+    data = int(input("메뉴 선택(1~6) : "))
+
+    if data == 1:
+        if isCheck == 0:
+            nums = input("숫자 2개 입력 : ").strip()
+            n, m = nums.split(",")
+            n = int(n)
+            m = int(m)
+            isCheck = True
+        else:
+            print("이미 입력값이 있습니다.")
+
+    elif data == 2 and isCheck:
+        print(f"{n} + {m} = {n+m}")
+
+    elif data == 3 and isCheck:
+        print(f"{n} + {m} = {n-m}")
+
+    elif data == 4 and isCheck:
+        print(f"{n} * {m} = {n*m}")
+
+    elif data == 5 and isCheck:
+        print(f"{n} / {m} = {n/m}")
+
+    elif data == 6 and isCheck:
+        print("나의 계산기 프로그램을 종료합니다.")
+        break
+
+    else:
+        print("메뉴 선택 오류")
