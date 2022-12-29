cd06d8c14043c185df271e90e442185befe22ff6
hongyeol <napolehong92@naver.com>
2022-12-29 22:37:30 +0900

    2480

--- BaekJoon_algorithm/2480.py
+++ BaekJoon_algorithm/2480.py
@@ -1,10 +1,24 @@
+a,b,c = map(int, input().split())
 
+if a==b==c:
+    print((a * 1000)+10000)
+elif a in (b,c):
+    print((a*100)+1000)
+elif b in (a,c):
+    print((b*100)+1000)
+else :
+    print(max([a,b,c])*100)
+    
 
-# 3개가 같은경우
-    #같은 개수를 파악하고, 그것이 무엇인지를 알기
-# 2개가 같은경우
-# 
-# 1개만 같은경우 
-    #제일 큰수
-    #max()
+# a, b, c = map(int, input().split())
 
+# if a == b == c:
+#     print(10000+a*1000)
+# elif a == b:
+#     print(1000+a*100)
+# elif a == c:
+#     print(1000+a*100)
+# elif b == c:
+#     print(1000+b*100)
+# else:
+#     print(100 * max(a,b,c))
