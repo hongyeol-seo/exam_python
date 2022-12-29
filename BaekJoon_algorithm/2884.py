358c0046c4a8b34f81c2e1070fa5db6f7ae3a6a8
hong <napolehong92@naver.com>
2022-12-29 13:13:51 +0900

    1229 백준

--- BaekJoon_algorithm/2884.py
+++ BaekJoon_algorithm/2884.py
@@ -11,3 +11,15 @@ elif H == 0 and  M < 45 :
 
 else :
     print(H-1,M+15)
+
+# H, M = map(int, input().split())
+
+# if M < 45 :	# 분단위가 45분보다 작을 때 
+#     if H == 0 :	# 0 시이면
+#         H = 23
+#         M += 60
+#     else :	# 0시가 아니면 (0시보다 크면)
+#         H -= 1	
+#         M += 60
+        
+# print(H, M-45)	
