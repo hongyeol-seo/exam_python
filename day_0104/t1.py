5a2f672ef313d88bbfb91cfd9cbe4eb0c974b3bd
hong <napolehong92@naver.com>
2023-01-06 15:19:51 +0900

    20230106_프로젝트

--- day_0104/t1.py
+++ day_0104/t1.py
@@ -1,6 +1,9 @@
 # result2 = list(map((lambda x: x in range(3)))) 
 # print(result2)
 
+FILE = ''
+
+FILE = '../datafilsgapminder'
 
 result = list(map(lambda x : x, list(range(5,10))))  # 제곱시키기
 print(result)
