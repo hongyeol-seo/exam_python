32a42ceb3bb8e1cb8205a063d689e5a14055d172
hong <napolehong92@naver.com>
2023-01-09 09:07:14 +0900

    open_api

--- day_0107/ex_openAPI.py
+++ day_0107/ex_openAPI.py
@@ -1,16 +1,27 @@
-# Python3 샘플 코드 #
-
-
 import requests
-from bs4 import BeautifulSoup 
-url = 'http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getMinuDustFrcstDspth'
-params ={'serviceKey' : 'nYP3LqqwTg5GNS52Q4YW6OgTesn898EOaEGsWDGE4nFiAWypzAQyl5Yy80y1uqNdz9RDVTwnhUDaItKUkwHSKw==', 'returnType' : 'xml', 'numOfRows' : '100', 'pageNo' : '1', 'searchDate' : '2020-11-14', 'InformCode' : 'PM10' }
+# arr = 'http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty?stationName=종로구&dataTerm=month&pageNo=1&numOfRows=100&returnType=json&serviceKey=nYP3LqqwTg5GNS52Q4YW6OgTesn898EOaEGsWDGE4nFiAWypzAQyl5Yy80y1uqNdz9RDVTwnhUDaItKUkwHSKw%3D%3D'
+
+# response = requests.get(arr)
+# print(response.content)
+# # 일반 인증키
+# # (Encoding)
+# #nYP3LqqwTg5GNS52Q4YW6OgTesn898EOaEGsWDGE4nFiAWypzAQyl5Yy80y1uqNdz9RDVTwnhUDaItKUkwHSKw%3D%3D 
+
+url = 'http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty'
+api_decode = requests.utils.unquote(url)
+params ={'serviceKey' : api_decode, 'returnType' : 'xml', 'numOfRows' : '100', 'pageNo' : '1', 'sidoName' : '서울', 'ver' : '1.0' }
 
 response = requests.get(url, params=params)
+print("아래")
 print(response.content)
-# bs_obj = BeautifulSoup(response.content, "html.parser")
-# print(response.content)
-# print(bs_obj)
-# print(response.text)
 
-# print(response.dataTime)
+#url 방식
+
+# arr1 = 'http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getMinuDustWeekFrcstDspth?searchDate=2020-11-09&returnType=xml&serviceKey=nYP3LqqwTg5GNS52Q4YW6OgTesn898EOaEGsWDGE4nFiAWypzAQyl5Yy80y1uqNdz9RDVTwnhUDaItKUkwHSKw%3D%3D&numOfRows=100&pageNo=1'
+# response = requests.get(arr1)
+# print(response.content)
+
+url = 'http://openapi.seoul.go.kr:8088/sample/xml/CardSubwayStatsNew/1/5/20220301'
+
+response = requests.get(url)
+print(response.content)
